7a938d6156a761df010c13cbd0b8c087b4e7699c
Trung Hoang <sparkboldstudio@gmail.com>
2018-08-31 06:40:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-    BRANDS = BRANDS.uniq
+    brands_array << brand
+    BRANDS = brands_array.uniq
   end
 
   def cobble
